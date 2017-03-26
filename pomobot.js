ef00dd9003e22b48fa9a166e5b9f84931659f2a9
Owen Shen <owenshen24@gmail.com>
2017-03-26 19:43:55 +0000

    fixed Pomo Bot

--- pomobot.js
+++ pomobot.js
@@ -3,17 +3,19 @@ var clockdiv = document.querySelector('.clockdiv');
 var Minutes = document.querySelector('.Minutes');
 var Seconds = document.querySelector('.Seconds');
 var Resetbutt = document.getElementById('reset');
-var pause = false;
+var startover = false;
+var time = 25;
 
-function clock (val) {
+function clock(val) {
     var endtime = val;
     var timeinterval = setInterval(function() {
     var t = getTimeRemaining(endtime);
-    if (! pause) {
     Minutes.innerHTML = 'Minutes: '+t.minutes;
     Seconds.innerHTML = 'Seconds: '+t.seconds;
-    }
     if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+    if(startover === true){
       clearInterval(timeinterval);
     }
   },1000);
@@ -31,14 +33,25 @@ function getTimeRemaining(endtime) {
 }
 
 function startClock() {
-    var end = Date.now() + (25*60*1000);
+    startover = false;
+    var end = Date.now() + (time*60*1000);
     clock(end);
 }
 
 button.addEventListener("click", startClock);
 
 function reset() {
-    pause = true;
+    startover = true;
+    Minutes.innerHTML = 'Minutes: '+time;
+    Seconds.innerHTML = 'Seconds: '+0;
 }
 
 Resetbutt.addEventListener("click", reset);
+
+
+
+
+
+
+
+
