62e1646872891e2a0876a4cbd605346506e37caf
Owen Shen <owenshen24@gmail.com>
2017-03-26 01:11:37 +0000

    Updated now! Woohoo!

--- script.js
+++ script.js
@@ -1,100 +1,35 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
-
-var guesses = document.querySelector('.guesses');
-var lastResult = document.querySelector('.lastResult');
-var lowOrHi = document.querySelector('.lowOrHi');
-
-var guessSubmit = document.querySelector('.guessSubmit');
-var guessField = document.querySelector('.guessField');
-
-var guessCount = 1;
-var resetButton;
-
-function checkGuess() {
-  var userGuess = Number(guessField.value);
-  if (guessCount === 1) {
-    guesses.textContent = 'Previ: <br>';
-  }
-  guesses.textContent += userGuess + "<br>";
- 
-  if (userGuess === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.style.backgroundColor = 'green';
-    lowOrHi.textContent = '';
-    setGameOver();
-  } else if (guessCount === 10) {
-    lastResult.textContent = '!!!GAME OVER!!!';
-    setGameOver();
-  } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.style.backgroundColor = 'red';
-    if(userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if(userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
-    }
-  }
- 
-  guessCount++;
-  guessField.value = '';
-  guessField.focus();
-    }
-    
-guessSubmit.addEventListener('click', checkGuess);
-    
-function setGameOver() {
-  guessField.disabled = true;
-  guessSubmit.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
-  document.body.appendChild(resetButton);
-  resetButton.addEventListener('click', resetGame);
-}
-
-function resetGame() {
-  guessCount = 1;
-
-  var resetParas = document.querySelectorAll('.resultParas p');
-  for (var i = 0 ; i < resetParas.length ; i++) {
-    resetParas[i].textContent = '';
-  }
-
-  resetButton.parentNode.removeChild(resetButton);
-
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
-
-  lastResult.style.backgroundColor = 'white';
-
-  randomNumber = Math.floor(Math.random() * 100) + 1;
-}
-
-
-
-//Improved code goes here
-var dialoguetimes = 1;
+var times = 1;
 var answers = document.querySelector('.answers');
 var questions = document.querySelector('.questions');
+var Submit = document.querySelector('.Submit');
+var enter = document.querySelector('.enter');
+
+function clearThis(target) {
+        target.value= "";
+}
 
 function chat() {
-    var userGuess = String(guessField.value);
-    if (dialoguetimes === 1) {
-    answers.textContent += 'What do you want to do? | ';
-    questions.textContent = 'Great! Now what is step 1/3?';
-  }
-  answers.textContent += userGuess + '<br>';
+    var ans = String(enter.value);
+    if (times === 1) {
+    answers.innerHTML = 'What do you want to do? | ' + ans + '<br>';
+    questions.innerHTML = 'What is step 1 out of 3?';
+    clearThis(enter);
+    }
+    else 
+    if (times === 2) {
+    answers.innerHTML += ('Step 1 | ' + ans);
+    questions.innerHTML = 'What is step 2 out of 3?';
+    clearThis(enter);
+    }
+    times++;
 }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+enter.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        Submit.click();
+    }
+});
+
+Submit.addEventListener("click", chat);
+
