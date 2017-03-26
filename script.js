5aa55971c930bbc003b93c829300b71d06f3179d
Owen Shen <owenshen24@gmail.com>
2017-03-26 03:58:03 +0000

    Removed some comments

--- script.js
+++ script.js
@@ -82,33 +82,3 @@ function resetAll() {
 
 reset.addEventListener("click", resetAll);
 
-/* if (ans.toLowerCase().localeCompare(done) === 0) {
-      questions.innerHTML = 'Imagine that it is one week later and you failed to get anything done! What happened?';
-      nextstep++;
-      clearThis(enter);
-    }
-    else 
-    if (nextstep === 1) {
-      answers.innerHTML = 'Plausible failure: | ' + ans + '<br>';
-      questions.innerHTML = 'How can you prevent this from happening?';
-      nextstep++;
-      clearThis(enter);
-    }
-    else 
-    if (nextstep === 2) {
-      answers.innerHTML = 'Solution: | ' + ans + '<br>';
-      questions.innerHTML = 'Imagine that it is one week later and your solution failed. Are you surprised? (Reply "yes" or "no)';
-      nextstep++;
-      clearThis(enter);
-    }
-    if (nextstep === 3) {
-      if (ans.toLowerCase().localeCompare(yes) === 0) {
-        questions.innerHTML = 'Congrats! You have failure-proofed your plan!';
-      }
-      if (ans.toLowerCase().localeCompare(no) === 0) {
-        questions.innerHTML = 'What went wrong this time?';
-        nextstep = 1;
-      }
-      clearThis(enter);
-    }
-    else */
