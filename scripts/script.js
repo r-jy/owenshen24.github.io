9c9cd50ef9857a2d447475b2f95428f00f3b6594
Owen Shen <owenshen24@gmail.com>
2017-10-04 16:44:49 -0700

    Added new links, images, and revamped the CSS.

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 
+// Initial selection of Owen Icon to be greyed out.
+  $(".initial-desc").show();
+  $(".initial-selection").css('filter', 'grayscale(100%)');
 
-
-// What do I need?
-// When the button is clicked, it becomes not-transparent. Also, the corresponding paragraph is shown.
-// Also: When a button is clicked, cause all other buttons to be transparent, and hide all of the pragraphs first to ensure you only ever have 1 paragraph / button active at a time.
-
-// This will make sure that all buttons are greyscale, and then make the selected one colored.
 
   function selectButton() {
     // Alters the button color filters to show selection
     var clickedButton = $(this);
-    $(".image-button").css('filter', 'grayscale(100%)');
-    clickedButton.css('filter', 'grayscale(0%)');
+    $(".image-button").css('filter', 'grayscale(0%)');
+    clickedButton.css('filter', 'grayscale(100%)');
 
     // Shows the corresponding text box that goes with it
     var clickedButtonIndex = $(this).index();
@@ -23,10 +20,3 @@ $(document).ready(function() {
 
 
 })
-
-// clickedButton.style.filter = "grayscale(0%)";
-// var allButtons = clickedButton.siblings();
-// for (i = 0; i < allButtons.length; i++) {
-//  var selectedButton = allButtons[i];
-//  selectedButton.style.filter = "grayscale(0%)";
-// }
