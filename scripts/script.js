6e9fe436baf6fb59e607822599131a92cd146519
Owen <owenshen24@gmail.com>
2019-06-18 16:46:15 -0700

    Improved font size, UI, and made video firefox compatible

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,13 @@ $(document).ready(function() {
   // Hide the modal
   $(".modal").hide();
 
+  let selection = $(".initial-selection");
+
   function selectButton() {
     // Alters the button color filters to show selection
-    var clickedButton = $(this);
-    $(".image-button").css('filter', 'grayscale(0%)');
-    clickedButton.css('filter', 'grayscale(100%)');
+    selection.css('filter', '');
+    selection = $(this);
+    selection.css('filter', 'grayscale(100%)');
 
     // Shows the corresponding text box that goes with it
     var clickedButtonIndex = $(this).index();
