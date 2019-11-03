91b51a5f929714df6d3d759ac14dc013aa86772c
Owen <owenshen24@gmail.com>
2019-11-02 18:45:18 -0700

    Hotfix

--- secret/scripts/1.js
+++ secret/scripts/1.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 
   $("#run").click(function() {
     let ans = $("#ans").val();
+    if (ans === "{}") {
+        alert("🤔");
+    }
     if (ans != "{") {
-      alert("Error: {} expected.");
+      alert("Error: { expected.");
     }
     else if (ans === "{") {
       alert("Error: } expected.");
-    }
-    else if (ans === "{}") {
-      alert("🤔");
     }
   });
 
